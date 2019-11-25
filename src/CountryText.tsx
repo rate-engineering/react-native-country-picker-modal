c53beb544d529c7c0fa99ba81fb6c8041ece6244
Andre Khong <andrekhong@gmail.com>
2019-11-25 18:00:01 +0800

    Fix CountryText component styling

--- src/CountryText.tsx
+++ src/CountryText.tsx
@@ -2,12 +2,12 @@ import React, { ReactNode } from 'react'
 import { TextProps, Text } from 'react-native'
 import { useTheme } from './CountryTheme'
 
-export const CountryText = (props: TextProps & { children: ReactNode }) => {
+export const CountryText = ({ style, ...restProps }: TextProps & { children: ReactNode }) => {
   const { fontFamily, fontSize, onBackgroundTextColor } = useTheme()
   return (
     <Text
-      {...props}
-      style={{ fontFamily, fontSize, color: onBackgroundTextColor }}
+      {...restProps}
+      style={[{ fontFamily, fontSize, color: onBackgroundTextColor }, style]}
     />
   )
 }
