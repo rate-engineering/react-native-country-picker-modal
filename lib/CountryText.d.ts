0865aaef644b99ceb7c77dd49cd9051d2bb77546
Andre Khong <andrekhong@gmail.com>
2019-11-25 18:36:53 +0800

    Fix distributable files and linting errors

---
+++ lib/CountryText.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import { TextProps } from 'react-native';
+export declare const CountryText: ({ style, ...restProps }: TextProps & {
+    children: React.ReactNode;
+}) => JSX.Element;
